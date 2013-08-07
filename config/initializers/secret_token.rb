887106fd5cfdb517e56f344dfd3a1885ac9b5d06
SamMWBlake <sam.k.blake@gmail.com>
2013-08-07 11:01:23 -0500

    Generated Rails skeleton (w/ postgresql)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Binder::Application.config.secret_key_base = 'ebef945aa49d642f97d1bcb68a73c781a4a43283149c7d55f8965c35796ba4f22c3f98c6da6675d7895c46f637e31f922a61c306851dc8f49c2b14d486728ac8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Binder::Application.config.session_store :cookie_store, key: '_binder_session'
